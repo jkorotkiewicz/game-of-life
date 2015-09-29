4a862fec21e30d7ee58d95a8414ef5faf1c9faab
jkorotkiewicz <jkorotkiewicz+github@gmail.com>
2015-09-29 12:01:17 +0200

    Initial commit, something works

---
+++ game.js
@@ -0,0 +1,93 @@
+var x = 100;
+var y = 100;
+
+function main() {
+    var world = new Array(x);
+    for (var i = 0; i < x; i++) {
+        world[i] = new Array(y);
+    }
+    initiateBoard(world);
+    drawBoard(world);
+    drawSection();
+    for(var i = 0; i < 100; i++) {
+    	world = makeStep(world);
+    	drawBoard(world);
+    	drawSection();    	
+    }
+}
+
+function makeStep(world) {
+    var newWorld = new Array(x);
+    for (var i = 0; i < this.x; i++) {
+        newWorld[i] = new Array(this.y);
+    }
+    // debugger;
+    for (var i = 0; i < this.x; i++) {
+        for (var j = 0; j < this.y; j++) {        	
+        	newWorld[i][j] = checkNeighbours(world, i, j);
+        }
+    }
+    world = newWorld;
+    return newWorld;
+}
+
+function checkNeighbours(world, x, y) {
+
+    var n = 1;
+    var s = 1;
+    var e = 1;
+    var w = 1;
+    var counter = 0;
+    if (x == 0) w = 0;
+    if (x == this.x-1) e = 0;
+    if (y == 0) n = 0;
+    if (y == this.y-1) s = 0;
+    for (var i = x - w; i <= x + e ; i++) {
+        for (var j = y - n; j <= y + s ; j++) {        	
+            if(!(i==x && j==y)) counter += world[i][j];
+        }
+    }
+    // document.write(w + ' ' + n  + ' ' + e  + ' ' + s);
+    // document.write(' i = ' + i + ' j = ' + j + ' x = ' + x + ' y = ' + y + ' counter = ' + counter + '\n');
+    // alert('x = ' + x + ' y = ' + y + ' counter = ' + counter);
+    //if(counter < 2 || counter > 3) return 0;
+    // return counter;
+    if(world[x][y]) return(counter<2?0:counter>3?0:1);
+    else return(counter==3?1:0);
+}
+
+function initiateBoard(world) {
+    for (i = 0; i < x; i++) {
+        for (j = 0; j < y; j++) {
+            // world[i][j] = Math.floor(Math.random() + 0.2);
+            world[i][j] = 0;
+            if(i == 0 && j == 1) world[i][j] = 1;
+            if(i == 1 && j == 2) world[i][j] = 1;
+            if(i == 2 && j == 0) world[i][j] = 1;
+            if(i == 2 && j == 1) world[i][j] = 1;
+            if(i == 2 && j == 2) world[i][j] = 1;
+        }
+    }
+}
+
+function drawBoard(world) {
+    var dead = ' &middot';
+    var alive = ' 0';
+    for (i = 0; i < x; i++) {
+        document.write("<section >");
+        for (j = 0; j < y; j++) {
+            document.write(world[i][j] ? alive : dead);
+        }
+        document.write("</section>");
+    }
+}
+
+function drawSection() {
+    document.write("<section>");
+    for (i = 0; i <= x; i++) {
+        document.write("--");
+    }
+    document.write("</section>");
+}
+
+main();
